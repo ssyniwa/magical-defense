53acf97acdf132212509267c9e7415edb9e2c7bd
SyouyaNiwa <109267191+ssyniwa@users.noreply.github.com>
2026-07-28 13:17:31 +0900

    Remove unnecessary whitespace in app.py

--- app.py
+++ app.py
@@ -484,7 +484,6 @@ def next_turn():
             st.session_state.gold += 20
             st.session_state.score += 50
     st.session_state.enemies = surviving_enemies
-　　
     summoned_new_enemies = []
     for e in st.session_state.enemies:
         if e.get("name") == "ネクロマンサー":
